b3363f6baeef0d876f677df8a057abd67318d85a
Denis Rodionov <alax777kilden@yandex.ru>
2026-09-07 01:12:55 +0400

    DEN-45 Add Android and iOS CI quality gates

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,34 @@ plugins {
     alias(libs.plugins.kotlin.multiplatform) apply false
     alias(libs.plugins.kotlin.serialization) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.spotless)
 }
 
+spotless {
+    format("kotlin") {
+        target("**/*.kt")
+        trimTrailingWhitespace()
+    }
+    format("kotlinGradle") {
+        target("*.gradle.kts", "**/*.gradle.kts")
+        trimTrailingWhitespace()
+    }
+    format("misc") {
+        target(
+            ".gitattributes",
+            ".gitignore",
+            ".github/**/*.yml",
+            ".github/**/*.yaml",
+            "README.md",
+        )
+        targetExclude(
+            "**/.gradle/**",
+            "**/build/**",
+            "**/DerivedData/**",
+            "iosApp/iosApp.xcodeproj/project.pbxproj",
+            "**/Package.resolved",
+        )
+        trimTrailingWhitespace()
+        endWithNewline()
+    }
+}
